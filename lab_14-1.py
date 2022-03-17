affe93aa56abbef1eda4417397591939729253e7
P22inolan <p22inolan@fairfieldprep.org>
2022-03-17 09:58:59 -0400

    Submit 14-1 & 14-2 3/17

---
+++ lab_14-1.py
@@ -0,0 +1,21 @@
+# Author: IBN (AMDG) 3/17/2022
+
+import turtle
+
+window = turtle.Screen()
+turt = turtle.Turtle()
+
+window.setup(1000, 1000)
+window.screensize(500, 500)
+window.title("Lab 1")
+
+turt.forward(250)
+turt.right(90)
+turt.forward(100)
+turt.right(90)
+turt.forward(250)
+turt.right(90)
+turt.forward(100)
+turt.right(90)
+
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,19 @@
+# Author: IBN (AMDG) 3/17/2022
+
+import turtle
+
+window = turtle.Screen()
+turt = turtle.Turtle()
+
+window.setup(500, 500)
+window.screensize(400, 400)
+window.title("Lab 2")
+
+turt.right(60)
+turt.forward(100)
+turt.right(120)
+turt.forward(100)
+turt.right(120)
+turt.forward(100)
+
+window.mainloop()
